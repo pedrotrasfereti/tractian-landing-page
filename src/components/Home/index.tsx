227db0fa5af8115a237fc80d7b977b79a9c17c0d
Pedro Trasfereti <pedrotrasfereti@gmail.com>
2022-04-17 00:04:21 -0300

    feat: add illustration and fix z-index

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,34 +1,40 @@
 import React from "react";
 
+/* Assets */
+import Illustration from "./assets/stripe3.png";
+
 /* Styles */
 import "./styles.css";
 
 function Home() {
   return (
     <div className="Home" id="home">
-      <div className="Container" id="heading-container">
-        <h1 className="h1" id="title">
-          Online Monitoring and Asset Management in One Place
-        </h1>
-        <h4 className="h4" id="subtitle">
-          Avoid failures in your machines and make downtime a thing of the past
-          with TRACTIAN&apos;s predictive system.
-        </h4>
-        <button className="Button" id="schedule-demo-btn" type="button">
-          Schedule a demo
-        </button>
-        <p className="LinkParagraph">
-          Already a customer?
-          <span> </span>
-          <a href="http://">Access here.</a>
-        </p>
+      <div className="RowContainer">
+        <div className="ColumnContainer" id="heading-container">
+          <h1 className="h1" id="title">
+            Online Monitoring and Asset Management in One Place
+          </h1>
+          <h4 className="h4" id="subtitle">
+            Avoid failures in your machines and make downtime a thing of the
+            past with TRACTIAN&apos;s predictive system.
+          </h4>
+          <button className="Button" id="schedule-demo-btn" type="button">
+            Schedule a demo
+          </button>
+          <p className="LinkParagraph">
+            Already a customer?
+            <span> </span>
+            <a href="http://">Access here.</a>
+          </p>
+        </div>
+
+        <img className="Illustration" src={Illustration} alt="Illustration" />
       </div>
 
       <section className="Waves" id="bg-waves">
         <div className="Wave Wave1" />
         <div className="Wave Wave2" />
         <div className="Wave Wave3" />
-        <div className="Wave Wave4" />
       </section>
     </div>
   );
